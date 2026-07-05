9c434533f09d2adff1ef814367380247326b6e82
toufik401 <toufiiktito40@gmail.com>
2026-07-05 18:07:23 +0100

    Update .nexora.py للمواضيع.py

--- .nexora.py للمواضيع.py
+++ .nexora.py للمواضيع.py
@@ -1,6 +1,4 @@
-Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import streamlit as st
+import streamlit as st
 import json
 import os
 import requests
