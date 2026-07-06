e630566ba72217e724591e32865719bc43892de0
toufik401 <toufiiktito40@gmail.com>
2026-07-06 15:11:59 +0100

    Update .nexora.py للمواضيع.py

--- .nexora.py للمواضيع.py
+++ .nexora.py للمواضيع.py
@@ -98,7 +98,7 @@ elif st.session_state.page == "checkout":
         address = ""
         if "توصيل" in delivery:
             address = st.text_input("عنوان المنزل بالتفصيل")
-            total += 100 دج 
+            total += 100دج 
 
         submitted = st.form_submit_button("تأكيد الطلب النهائي")
         
