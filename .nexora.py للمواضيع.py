33a9c8600c347203bf1bbafa1462357a82947599
toufik401 <toufiiktito40@gmail.com>
2026-07-06 15:02:40 +0100

    Update .nexora.py للمواضيع.py

--- .nexora.py للمواضيع.py
+++ .nexora.py للمواضيع.py
@@ -90,7 +90,6 @@ if st.session_state.page == "shop":
 elif st.session_state.page == "checkout":
     st.title("📄 تأكيد الطلب")
   total = sum(st.session_state.data['products'][p]['price'] * q for p, q in st.session_state.cart.items())
-    
     with st.form("order_form"):
         name = st.text_input("الاسم ")
         phone = st.text_input("رقم الهاتف")
